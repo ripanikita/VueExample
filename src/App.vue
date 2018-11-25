1e54e385c15e6ae7005133e004a47b522abe8bb1
ripanikita <ripanikita@yandex.ru>
2018-11-25 19:22:12 +0300

    Добавлен компонент выбора времени

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
   <div>
     <add />
     <hr />
-    <list />
+    <filter1 />
     <hr />
-    <filter />
+    <list />
   </div>
 </template>
 
 <script>
   import add from './Add.vue'
   import list from './List.vue'
-  import filter from './Filter.vue'
+  import filter1 from './Filter.vue'
+
   export default {
     components: {
       add,
       list,
-      filter
+      filter1
     }
   }
 </script>
--- src/Filter.vue
+++ src/Filter.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
+    <h2>Редактировнаие времени</h2>
+    <ul>
+      <li v-for="time in timeArr">{{ time }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      timeArr:[]
+    }
+  },
+  mounted() {
+      let i;
+      for(i=1000; i<=1330; i+=15){
+        let x = String(i);
+        if(x.length<4){x = '0'+x}
+        if(Number(x[0])<3 && Number(x[2])<6){
+          let y = x[0]+x[1]+':'+x[2]+x[3];
+          this.timeArr.push(y);
+        }
+      }
+  }
 }
 </script>
 
